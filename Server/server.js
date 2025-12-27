ffa5f77c0d807a5ab2faea812a759d8097f75c16
Avnish Kumar <126278725+AvnishKr01@users.noreply.github.com>
2025-12-27 21:00:42 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -35,10 +35,11 @@ module.exports = app;
 // ✅ Only run app.listen() if running locally
 if (require.main === module) {
   const port = process.env.PORT || 10000;
-  app.listen(port, 0.0.0.0, () => {
-    console.log(`Server running on port ${port}`);
-  });
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
+});
 }
+
 
 
 
